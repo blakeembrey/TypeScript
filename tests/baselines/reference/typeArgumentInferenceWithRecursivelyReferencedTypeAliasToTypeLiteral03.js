34f65f4631ad8bc2a723712c3fc288306208154b
Daniel Rosenwasser <drosen@microsoft.com>
2015-08-26 10:55:31 -0700

    Accepted baselines.

---
+++ tests/baselines/reference/typeArgumentInferenceWithRecursivelyReferencedTypeAliasToTypeLiteral03.js
@@ -0,0 +1,13 @@
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToTypeLiteral03.ts]
+type TreeNode = {
+    leftRight: [TreeNode, TreeNode];
+};
+
+function foo<U>(x: TreeNode) { }
+var n: TreeNode;
+foo(n);
+
+//// [typeArgumentInferenceWithRecursivelyReferencedTypeAliasToTypeLiteral03.js]
+function foo(x) { }
+var n;
+foo(n);
